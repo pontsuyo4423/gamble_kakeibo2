df7c5365a034867786260e89d53500185aca73c3
pontsuyo4423 <tsuyopi4423@gmail.com>
2024-02-13 11:15:54 +0900

    その他の装飾

--- app/controllers/incomes_controller.rb
+++ app/controllers/incomes_controller.rb
@@ -1,5 +1,5 @@
 class IncomesController < ApplicationController
-  before_action :authenticate_user!, except: [:index, :show]
+  before_action :authenticate_user!
   before_action :set_income, only: [:edit, :update]
 
   def index
--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,4 +1,5 @@
 class PaymentsController < ApplicationController
+  before_action :authenticate_user!
   before_action :set_income, only: [:edit, :update]
 
   def index
